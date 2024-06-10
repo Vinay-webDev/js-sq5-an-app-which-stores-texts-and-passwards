fda6e964a78ae7d36d1fa3b7ca82259cfd9bfbf7
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-10 22:31:47 +0530

    3. section view2

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//1.///1.///1.////
+const view1 = document.getElementById("view1");
+const view2 = document.getElementById("view2");
+function signIn() {
+    view1.style.display = "none";
+    view2.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
