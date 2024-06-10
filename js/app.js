859406239bb91d7c97e433170aa54251845eef34
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-10 23:05:50 +0530

    5. set password functionality😀😀😀😭

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 //1.///1.///1.////
+let password = [];
 const view1 = document.getElementById("view1");
 const view2 = document.getElementById("view2");
 function signIn() {
     view1.style.display = "none";
     view2.style.display = "block";
 }
+///2. section view2 /// 2. section view2 //// 2. section view2 //////
 
 
 
+function setPass() {
+    let newPass = document.getElementById("new-pass").value;
+    let rePass = document.getElementById("re-pass").value;
+    //console.log(newPass);
+    //console.log(rePass);
+    if (newPass === rePass) {
+        password.push(newPass);
+    }
+    else {
+        alert("password don't match!");
+        newPass = "";
+        rePass = "";
+    }
+    console.log(password);
+}
 
 
 
