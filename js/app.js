5d3ce65950182163e63b2a9cc21a113f26cd542a
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-11 23:35:58 +0530

    11. log-in function😀😀

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let password = [];
 const view1 = document.getElementById("view1");
 const view2 = document.getElementById("view2");
+const view3 = document.getElementById("view3");
 function newUser() {
     if(localStorage.length === 0) {
         view1.style.display = "none";
@@ -25,6 +26,7 @@ function setPass() {
     }
     else if (newPass === rePass) {
         password.push(newPass);
+        /* we need to convert data JSON*/
         localStorage.setItem("mystore1", JSON.stringify(password));
         view2.style.display = "none";
         view1.style.display = "block";
@@ -38,9 +40,28 @@ function setPass() {
     }
     console.log(password);
 }
+//3. section view3 ///3. section view3 ///3. section view3
 
-
-
+function logIn() {
+    let logInPassword = document.getElementById("login-pass").value;
+    /* need to get the log-in password from mystore1 */
+    /* remember we need to parse the JSON data from localStorage */
+    //const password = localStorage.getItem("mystore1") 
+    const localData = JSON.parse(localStorage.getItem("mystore1"));
+    //console.log(localData);
+    const password = localData[0];
+    //console.log(password);
+    if (logInPassword === "") {
+        alert("please enter password!");
+    }
+    else if (logInPassword === password) {
+        view1.style.display = "none";
+        view3.style.display = "block";
+    }
+    else {
+        alert("wrong password!");
+    }
+}
 
 
 
